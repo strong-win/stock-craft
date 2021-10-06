b060ff52f2bc976a6e00387251d398b057ea36c3
Lee Ho Jun <32982728+beeetea@users.noreply.github.com>
2021-10-06 05:46:16 +0000

    fix: data_constant and stock_generator

--- data/stock_generator.py
+++ data/stock_generator.py
@@ -1,15 +1,19 @@
-import numpy as np
+import os
+#import numpy as np
+import pandas as pd
 from pykrx import stock
-from random import sample, radnint
+from random import sample, randint
+from data_constant import KOSPI_200, TICKER_NAME, DATA_PATH
+
+# TO-DO: 예외처리하기!
 
 def get_data_by_len(quantity: int = 5, duration: int = 30) -> 'numpy.ndarray':
 	target_tickers = sample(KOSPI_200, quantity)
-
+	res = []
 	for ticker in target_tickers:
-		# 가능한 범위를 구하고
-		
-		# 그 중에서 하나 샘플추출
+		df = pd.read_csv(os.path.join(DATA_PATH, TICKER_NAME[ticker]))
+		max_val = len(df) - duration + 1
+		target_idx = randint(0, max_val-1)
+		res.append(df[target_idx:target_idx+1, :])
 
-		# 나온 결과를 기존의 Data에 Concat
-
-	# for loop이 종료된 후에 완성된 3darray() 를 반환
+	return res
