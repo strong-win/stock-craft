2b2b3cf3689646461289a2bf881eb552525afaad
beeetea <relativity1915@gmail.com>
2021-10-25 16:48:42 +0900

    fix: dojo.py to scrapping exchange_currency

--- data/dojo.py
+++ data/dojo.py
@@ -1,4 +1,8 @@
 from pykrx import stock
+from forex_python.converter import CurrencyRates
+from easy_exchange_rates import API as ExchangeAPI
+from data_constant import START_DATE, END_DATE
+from datetime import datetime, timedelta
 import pandas as pd
 import os
 import glob
@@ -33,5 +37,24 @@ import glob
 procedure that returns kospi200 indicies
 """
 # TO-DO : add exchange_rate between 2000-01-04 and 2021-09-17
-df = stock.get_index_ohlcv_by_date("20000101", "20210917", "1001")
-df.to_csv('./stock_data_0922/kospi.csv')
+# df = stock.get_index_ohlcv_by_date("20000101", "20210917", "1001")
+# df.to_csv('./stock_data_0922/kospi.csv')
+
+
+"""
+procedure that returns USD-KRW Exchange Rate indicies
+"""
+# TO-DO : get USD-KRW Exchange Rate by our START_DATE and END_DATE
+c = CurrencyRates()
+df = pd.DataFrame(pd.date_range(start='1/1/2000 10:00:00', end='9/17/2021 11:00:00', freq='D'), columns=['DateTime'])
+
+def get_rate(x):
+    try:
+        op = c.get_rate('CAD', 'USD', x)
+    except Exception as re:
+        print(re)
+        op=None
+    return op
+
+df['Rate'] = df['DateTime'].apply(get_rate)
+print(df)
