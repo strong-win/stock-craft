e91656f2b46224cfe9e5c34f3e3c46b53649a319
beeetea <relativity1915@gmail.com>
2021-11-12 21:22:27 +0900

    add: trend on price generator, comment on APIs TO-BE implemented on market.py | fix: CorpEventInfo schema on schema.py

--- models/market.py
+++ models/market.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI # Serving
+from fastapi.responses import JSONResponse
 from conn import client
+from schema import GameInfo, CorpInfo, CorpResInfo
+from math import log2
 # from TIMEBAND.core import TIMEBANDCore # ML과 통신
 import pymongo # NoSQL 연동
 import json
@@ -13,6 +16,12 @@ MODEL_PATH = "..."  # Model `.pt` 파일이 있는 경로
 
 app = FastAPI()
 
+def add_buy_trend(price: int, volume: int):
+	return round(log2(volume) * stock_round(price))
+
+def add_sell_trend(price: int, volume: int):
+	return -1 * round(log2(volume) * stock_round(price))
+	
 def stock_round(price):
 	"""
 	return rounded price of KOSPI market
@@ -33,22 +42,20 @@ def stock_round(price):
 		base = 1000
 	return base * round(price/base)
 
-def get_current_price(stock_info: list[str], result_df: 'pd.DataFrame', target_stocks: int = 5, target_days: int = 3) -> dict:
+def get_current_price(stock_info: list[str], result_df: 'pd.DataFrame', target_stocks: int = 5, target_days: int = 3, buying_volume: int, selling_volume: int) -> dict:
 	"""
 	NOTICE: order of stock_info is ASC
 	`result_df`의 데이터를 바탕으로 샘플링을 진행한 후, (2*days, 2*stock) 형태의 dataframe을 반환하는 function
 	"""
-	# TODO: 숫자 인덱스 ticker로 변경하기
 	res = {k:{} for k in sorted(stock_info)}
 
 	target_df = result_df
 	for i in range(1, target_stocks + 1):
 		price_list = []
 		for j in range(target_days):
-			# TODO: 매수/메도세 반영 in upper, lower
-			upper = target_df['close'+str(i)+'_upper'][j]
-			lower = target_df['close'+str(i)+'_lower'][j]
-			target_range = upper - lower
+			upper = target_df['close'+str(i)+'_upper'][j] + add_sell_trend(target_df['close'+str(i)+'_upper'][j], selling_volume)
+			lower = target_df['close'+str(i)+'_lower'][j] + add_buy_trend(target_df['close'+str(i)+'_lower'][j], buying_volume)
+			target_range = abs(upper - lower)
 			z_score = np.random.normal()			
 			final_price = stock_round(0.25 * target_range  * z_score + (upper + lower) // 2)
 			price_list.append(final_price)
@@ -60,26 +67,63 @@ def get_current_price(stock_info: list[str], result_df: 'pd.DataFrame', target_s
 	return res
 
 @app.post("/model")
-def init_model():
+def init_model(game_info: GameInfo):
 	"""
-	이미 학습된 모델을 1:1 매칭한 후, 초기 데이터를 반환하는 API (생성자)
+	이미 학습된 모델을 1:1 매칭한 후, 초기 데이터를 반환하는 API (constructor)
 	"""
-	pass
-	# config = CONFIG_PATH
-	# Core = TIMEBANDCore()
-	# model = torch.load('path')
-	# predict_data, predict_band = Core.predicts(data, model)
+	try:
+	# 1. 이미 있는 모델과 gameId를 매칭
+
+	# 2. 매칭된 정보를 dict타입으로 요약
+
+	# 3. 생성된 정보를 MongoDB에 document로 생성
+
+	# 4 .처음 생성된 주가를 가져와서 `totalChart`에 넣은 후 최종 데이터를 반환
+
+		return JSONResponse({"status": "OK"})
+	except Exception as e:
+		return JSONResponse({'gameId': game_info_with_event.gameId, "status": "Error : {}".format(e)})
+
 
 @app.put("/model")
-def update_model():
+def update_model(game_info_with_event: CorpEventInfo):
 	"""
 	game_id가 주어졌을 때, 해당 id를 바탕으로 새로운 데이터를 주는 API
 	"""
-	pass
+	try:
+	# 1. gameId에 해당하는 모델을 찾고, 이를 CONFIG_PATH로 지정
+
+	# 2. TIMEBANDCore를 선언하고, torch.load를 한 후, Core.predict를 진행
+	"""
+	이전 회의에서 나왔던 Protocol psuedo-code:
+	MODEL_PATH = mongoDB.find(gameId)
+	config = local.find(gameId)
+	Core = TIMEBANDCore(config)
+	model = torch.load(MODEL_PATH)
+	predict_data, predict_band = Core.predicts(data, model)
+	"""
+	# 3. 찾은 predict_data를 기반으로 get_current_price 진행(예측)
+
+	# 4. 예측된 결과를 반환
+
+		return JSONResponse({"status": "OK"})
+	except Exception as e:
+		return JSONResponse({'gameId': game_info_with_event.gameId, "status": "Error : {}".format(e)})
 
 @app.delete("/model")
-def delete_model():
+def delete_model(game_info: GameInfo):
 	"""
 	게임이 끝나고 model과의 관계를 삭제하는 API
 	"""
-	pass
+	try:
+	# 1. gameId에 해당하는 모델을 찾기
+
+	# 2. MongoDB에서 해당하는 value에 해당하는 document 제거
+
+	# 3. 추가적으로 이번 게임에서 활용된 경로, config 등의 의존성 제거
+
+	# 4. 성공 시그널: {gameId: str, status: 'OK'} 을 return
+
+		return JSONResponse({'gameId':game_info.gameId, 'status': "OK"})
+	except Exception as e:
+		return JSONResponse({'gameId':game_info.gameId, "status": "Error : {}".format(e)})
--- models/schema.py
+++ models/schema.py
@@ -3,13 +3,27 @@ from pydantic import BaseModel
 
 # used for req in POST
 class GameInfo(BaseModel):
-	"gameId": str
+	gameId: str
 
 # used for res in POST
 class CorpInfo(BaseModel):
-	"corpId": str
-	"totalChart": Optional[List[int]] = None
+	corpId: str
+	totalChart: Optional[List[int]] = None
 
 # used for req in PUT
+class CorpMarketInfo(BaseModel):
+	increment: int
+	buyingVolume: int
+	sellingVolune: int
+
+class CorpEventInfo(BaseModel):
+	gameId: str
+	event: Dict[str, CorpMarketInfo]
+
+# used for res in PUT
+class CorpValueInfo(BaseModel):
+	corpId: Dict[str, Union[int, List[int]]]
+
 class CorpResInfo(BaseModel):
-	"corpId": Dict[str, Union[int, List[int]]
+	gameId: str
+	stock: Dict[str, CorpValueInfo]
