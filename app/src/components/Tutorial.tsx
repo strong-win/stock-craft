06cd5121e9263f2aac4604224e20d1f398242e82
GyuZzang <tndud0606@gmail.com>
2021-11-22 23:57:03 +0900

    feat(tutorial): 발표용 튜토리얼 추가

--- app/src/components/Tutorial.tsx
+++ app/src/components/Tutorial.tsx
@@ -7,22 +7,30 @@ import {
   CarouselCaption,
 } from "reactstrap";
 
-import AssetTest from "../assets/image.png";
+import tutorial1 from "../assets/tutorial1.png";
+import tutorial2 from "../assets/tutorial2.png";
+import tutorial3 from "../assets/tutorial3.png";
+import tutorial4 from "../assets/tutorial4.png";
 const items = [
   {
-    src: AssetTest,
-    altText: "Slide 1",
-    caption: "Slide 1",
+    src: tutorial1,
+    altText: "",
+    caption: "",
   },
   {
-    src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    altText: "Slide 2",
-    caption: "Slide 2",
+    src: tutorial2,
+    altText: "",
+    caption: "",
   },
   {
-    src: "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    altText: "Slide 3",
-    caption: "Slide 3",
+    src: tutorial3,
+    altText: "",
+    caption: "",
+  },
+  {
+    src: tutorial4,
+    altText: "",
+    caption: "",
   },
 ];
 
@@ -76,11 +84,13 @@ const Tutorial = () => {
         />
         {slides}
         <CarouselControl
+          className="carouselControl"
           direction="prev"
           directionText=""
           onClickHandler={previous}
         />
         <CarouselControl
+          className="carouselControl"
           direction="next"
           directionText=""
           onClickHandler={next}
