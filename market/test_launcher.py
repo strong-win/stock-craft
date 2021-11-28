14328f548d050bf62173640f4da9766f3e35715e
Sangil Han <handal__@naver.com>
2021-11-28 15:54:58 +0900

    update: launch by pickle

--- market/test_launcher.py
+++ market/test_launcher.py
@@ -1,4 +1,15 @@
+import pickle
 from libs.models.launcher import main
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    CORE_PATH = "models/sample.pkl"
+    with open(CORE_PATH, mode="rb") as f:
+        model = pickle.load(f)
+
+    line, band = model.predicts()
+    print(line)
+    print(band)
+
+    # main()
